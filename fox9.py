963ae210b174bb8e5c09976127aa933da90d33e3
nure101 <58121428+nure101@users.noreply.github.com>
2023-07-02 15:06:11 -0500

    Update fox9.py

--- fox9.py
+++ fox9.py
@@ -3,10 +3,14 @@
 
 import requests
 from bs4 import BeautifulSoup 
-import datetime
-from datetime import datetime
+import os
+from twilio.rest import Client
 
 
+account_sid = os.environ.get('account_sid')
+auth_token = os.environ.get('auth_token')
+my_number = os.environ.get('my_number')
+twilio_number = os.environ.get('my_twilio_number')
 
 url = "https://www.fox9.com/"
 html = requests.get(url)
@@ -15,51 +19,7 @@ s = BeautifulSoup(html.content, 'html.parser')
 #print(s.title.string)
 words_to_watch = ["severe","fire","murder","tornado","wreckage","crash","killed","injured","dies","shooting","hurt","dead","drunk","crash","terrorized","severe"]
 
-curretn_date = datetime.date(datetime.now())
-
-#returns headlines that have the above words in them 
-
-    # def what():
-    #     job_title = s.find_all('h3')#class_="content article-list")
-    #     check = set()
-    #     for i in job_title:
-    #         i = i.text
-    #         for word in words_to_watch:
-    #             if word in i:
-    #                 check.add(i)  
-        
-    #     for i in check:
-    #         print (i)
-
-    #     #return check
-    # what()
-
-# check the time 
-    # print(curretn_date)
 happining =  set()
-
-
-
-    # def what():
-    #     job_title = s.find_all('h3')#class_="content article-list")
-    #     check = set()
-    #     for i in job_title:
-    #         #find time 
-    #         try:
-    #             time = i.parent.parent.find('time').text
-    #             if 'mins' in time and int(time[0]) < 59:
-    #                 happining.append(time)
-        
-
-            
-            
-    #         except AttributeError:
-    #             continue
-            
-    # what()
-    # print (happining)
-    
-#combine news headlines and time 
 
 def what():
     job_title = s.find_all('h3')#class_="content article-list")
@@ -74,15 +34,15 @@ def what():
                 for word in words_to_watch:
                     if word in i.text:
                         happining.add(headline)
-    
-
-        
-        
         except AttributeError:
             continue
         
 what()
-
+client = Client(account_sid, auth_token)
 for i in happining:
-    print(i)
-
+    client.messages.create(
+            to = my_number,
+            from_ = twilio_number,
+        # max char Twilio can send: 72
+            body = i[0:72]
+        )
